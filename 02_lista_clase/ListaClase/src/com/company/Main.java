31d861a4d56c1e873b7b5b1d93e57c53c72ba23a
Unknown <tehenua@gmail.com>
2016-11-14 21:28:41 +0100

    guardando cambios

--- 02_lista_clase/ListaClase/src/com/company/Main.java
+++ 02_lista_clase/ListaClase/src/com/company/Main.java
@@ -1,8 +1,35 @@
 package com.company;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+
+        java.io.BufferedReader br = new java.io.BufferedReader(new java.io.InputStreamReader(System.in));
+
+        String[] lista = new String[5];
+
+        for (int i = 0; i < lista.length; i++) {
+            System.out.println("Escribe el nombre y apellidos");
+            lista[i] = br.readLine().toUpperCase();
+        }
+
+        String b;
+        do{
+            System.out.println("Escribe la primera letra para buscar o FIN");
+            b = br.readLine().toUpperCase();
+            if(b.equals("FIN")){
+                System.out.println("AGUR");
+            }else{
+                System.out.println(buscar(lista));
+            }
+        }while (!b.equals("FIN"));
+
+    }
+
+    public static String buscar(String[] lista){
+
+        return "";
     }
 }
