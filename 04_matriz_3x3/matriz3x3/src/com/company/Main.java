9762dff98f3afd2e5b1f23b6e4bc8fa490160d77
TeHenua <tehenua@gmail.com>
2016-11-17 21:16:34 +0100

    proyecto creado

---
+++ 04_matriz_3x3/matriz3x3/src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Escribe un valor");
+        int[][][] matriz = new int[3][3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    matriz[i][j][k] = Integer.parseInt(br.readLine());
+                }
+            }
+        }
+        System.out.format("",)
+    }
+}
