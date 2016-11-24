5255150c977520098a775f80de9ef7d0a008ea44
Unknown <tehenua@gmail.com>
2016-11-24 21:37:58 +0100

    ejercicio terminado

--- 05_matriz_10x10/Matriz10x10/src/com/company/Main.java
+++ 05_matriz_10x10/Matriz10x10/src/com/company/Main.java
@@ -1,9 +1,30 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Random r = new Random();
+
+        int[][] matriz = new int[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                matriz[i][j] = r.nextInt(100);
+            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.format("%5d ", matriz[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
